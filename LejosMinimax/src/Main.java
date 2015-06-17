18d6b98084a6be7b87f9ca468de3cc80e63ee53a
andrevbampi <andrevbampi@gmail.com>
2015-06-16 21:44:11 -0300

    aaaaaaa
    aaaaaaa

---
+++ LejosMinimax/src/Main.java
@@ -0,0 +1,39 @@
+import lejos.nxt.Button;
+import lejos.nxt.ColorSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (!Button.ENTER.isPressed());
+		
+		ColorSensor color = new ColorSensor(SensorPort.S1);
+		
+//		Motor.A.rotate(360);
+		
+		Motor.B.rotate(65);
+		
+		Thread.sleep(3000);
+		
+		Motor.B.rotate(-65);
+
+		Thread.sleep(3000);
+		
+		int giro = 10;
+		int giroMotorB = 5;
+		
+		while (color.getColorID() != 0) {
+			Motor.B.rotate(giro);
+			
+			giroMotorB += giro;
+		}
+		
+		System.out.println("giro: " + giroMotorB);
+
+		while (!Button.ENTER.isPressed());
+	}
+
+}
